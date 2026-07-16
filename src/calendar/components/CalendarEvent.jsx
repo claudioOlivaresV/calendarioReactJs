e164af546ded7ca75eb82b10d4d0f7c95737c270
Claudio Olivares <ca.olivares91@gmail.com>
2026-07-16 17:53:53 -0400

    feat: mejora diseno

--- src/calendar/components/CalendarEvent.jsx
+++ src/calendar/components/CalendarEvent.jsx
@@ -4,9 +4,7 @@ export const CalendarEvent = ({ event }) => {
   const { title, user } = event;
   return (
     <>
-      <strong>{title}</strong>
-      <br></br>
-      <span>{user.name}</span>
+      <strong>{title}</strong>-<span>{user.name}</span>
     </>
   );
 };
